905aa7ba2775674b1664b44b441a11ac82bc67d3
Sudarshan Dodiya <69633295+sudarshandodiya@users.noreply.github.com>
2026-05-06 22:26:52 +0530

    Add back test options

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,11 @@ android {
     buildFeatures {
         compose = true
     }
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
